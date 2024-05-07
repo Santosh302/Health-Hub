99bb950b8b4573cc4c0581c979d0a46c68f34403
santosh302 <santoshkumarsy05@gmail.com>
2024-05-07 14:15:06 +0530

    server file done

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const app = express();
+import { v2 } from 'cloudinary';
+import Razorpay from 'razorpay';
 
-const PORT = process.env.PORT || 3000;
+import app from './app.js';
+import connectToDB from './configs/dbConn.js';
 
-app.get('/',(req, res)=>{
-    res.send('Hello World! santosh');
+// Cloudinary configuration
+v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Razorpay configuration
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, async () => {
+  // Connect to DB
+  await connectToDB();
+  console.log(`App is running at http://localhost:${PORT}`);
 });
