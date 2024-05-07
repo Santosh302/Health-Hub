437e1044245d510974cadda78781c60fce29bdd3
santosh302 <santoshkumarsy05@gmail.com>
2024-05-07 14:11:48 +0530

    data base connection config file and .env file example have done

---
+++ server/configs/dbConn.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+const connectToDB= async () =>{
+    try {
+        const { connection } = await mongoose.connect(
+            process.env.MONGO_URI || `mongodb://127.0.0.1:27017/health_hub`
+        );
+
+        if(connection){
+            console.log(`connected to MongoDB: ${connection.host}`);
+        }
+    } catch(error){
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+export default connectToDB;
